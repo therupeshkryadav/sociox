a1c94bf247b3f9024ecbd4b087e6d81f7011f1b2
therupeshkryadav <rupesh.official484@gmail.com>
2023-12-08 01:02:45 +0530

    testing

--- app/src/main/java/com/example/socialbundle/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/socialbundle/Fragments/HomeFragment.kt
@@ -6,12 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.socialbundle.Adapter.MyAdapter
 import com.example.socialbundle.R
 import com.example.socialbundle.Stories
 import com.example.socialbundle.databinding.FragmentHomeBinding
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class HomeFragment : Fragment() {
 
